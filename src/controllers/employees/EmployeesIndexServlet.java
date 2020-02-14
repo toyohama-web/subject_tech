a1d06f43c857911b611a6235526cf78fdcadf273
takeru.sato <the_toyohama_flying_corps@hotmail.com>
2020-02-15 02:01:10 +0900

    Add Employee create

--- src/controllers/employees/EmployeesIndexServlet.java
+++ src/controllers/employees/EmployeesIndexServlet.java
@@ -26,15 +26,16 @@ public class EmployeesIndexServlet extends HttpServlet {
         EntityManager em = DBUtil.createEntityManager();
 
         int page = 1;
-        try {
+        try{
             page = Integer.parseInt(request.getParameter("page"));
-        } catch(NumberFormatException e) {}
+        } catch(NumberFormatException e) { }
         List<Employee> employees = em.createNamedQuery("getAllEmployees", Employee.class)
-                .setFirstResult(15 * (page - 1))
-                .setMaxResults(15)
-                .getResultList();
+                                     .setFirstResult(15 * (page - 1))
+                                     .setMaxResults(15)
+                                     .getResultList();
 
-        long employees_count = (long)em.createNamedQuery("getEmployeesCount", Long.class).getSingleResult();
+        long employees_count = (long)em.createNamedQuery("getEmployeesCount", Long.class)
+                                       .getSingleResult();
 
         em.close();
 
@@ -44,7 +45,6 @@ public class EmployeesIndexServlet extends HttpServlet {
         if(request.getSession().getAttribute("flush") != null) {
             request.setAttribute("flush", request.getSession().getAttribute("flush"));
             request.getSession().removeAttribute("flush");
-
         }
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/index.jsp");
